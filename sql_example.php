ccbd46a6d49144176bd1b3fe56fe46d8d4e72d75
Zmalski <zacharymaludzinski@gmail.com>
2020-03-18 12:12:53 -0700

    Fixed example file for handy reference

--- sql_example.php
+++ sql_example.php
@@ -4,14 +4,14 @@ include 'db_connection.php';
 $conn = OpenCon();
 
 echo "Connected Successfully <br>";
-$query = "SELECT name, age FROM person";
+$query = "SELECT first_name, age FROM users";
 $result = $conn->query($query);
 CloseCon($conn);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"]. " - Age: " . $row["age"]."<br>";
+        echo "Name: " . $row["first_name"]. " - Age: " . $row["age"]."<br>";
     }
 } else {
     echo "0 results";
