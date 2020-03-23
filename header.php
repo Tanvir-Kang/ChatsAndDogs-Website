43d400dc2976c3e7fe686da906a6649377d1a88f
Zmalski <zacharymaludzinski@gmail.com>
2020-03-22 21:39:30 -0700

    Cleaned up header code a bit

--- header.php
+++ header.php
@@ -1,62 +1,40 @@
-<?php if (isset($_SESSION['login'])) : ?>
-	<header>
-	<a href="feed.php"><img id="logo" src="images/logo_text.PNG" /></a>
-	<div id="login">
-		<a href="logout.php">
-			<p>
-				Logout
-			</p>
-		</a>
-	</div>
-
-	<nav>
-		<ul class="navbar">
-			<a href="feed.php">
-				<li>
-					Chats
-				</li>
-			</a>
-			<a href="feed.php">
-				<li>
-					Advice
-				</li>
-			</a>
-		</ul>
-
-	</nav>
-</header>
-
-
-<?php else : ?>
 <header>
 	<a href="feed.php"><img id="logo" src="images/logo_text.PNG" /></a>
-	<div id="login">
-		<a href="login.php">
-			<p>
-				Login
-			</p>
-		</a>
-		<a href="signup.php">
-			<p style="border-top: solid black 1px;">
-				Sign Up
-			</p>
-		</a>
-	</div>
-
-	<nav>
-		<ul class="navbar">
-			<a href="feed.php">
-				<li>
-					Chats
-				</li>
+	<?php if (isset($_SESSION['login'])) : ?>
+		<div id="login">
+			<a href="logout.php">
+				<p>
+					Logout
+				</p>
 			</a>
-			<a href="feed.php">
-				<li>
-					Advice
-				</li>
+		</div>
+	<?php else : ?>
+		<div id="login">
+			<a href="login.php">
+				<p>
+					Login
+				</p>
 			</a>
-		</ul>
+			<a href="signup.php">
+				<p style="border-top: solid black 1px;">
+					Sign Up
+				</p>
+			</a>
+		</div>
+		<nav>
+			<ul class="navbar">
+				<a href="feed.php">
+					<li>
+						Chats
+					</li>
+				</a>
+				<a href="feed.php">
+					<li>
+						Advice
+					</li>
+				</a>
+			</ul>
 
-	</nav>
+		</nav>
 </header>
 <?php endif; ?>
